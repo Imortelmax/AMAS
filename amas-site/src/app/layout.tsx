92a917015cc88fb3ec11487253242936cd292bcf
Gauthier Watelain <gauthier.watelain@epitech.eu>
2026-02-18 10:19:00 +0100

    Page d'acceuil OK

--- amas-site/src/app/layout.tsx
+++ amas-site/src/app/layout.tsx
@@ -1,5 +1,12 @@
 import Navbar from "@/components/layout/navbar";
+import { Open_Sans } from 'next/font/google';
 import "./globals.css";
+
+const openSans = Open_Sans({
+  subsets:["latin"],
+  display: "swap",
+  variable: "--font-open-sans",
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="fr">
+    <html lang="fr" className={openSans.variable}>
       <body>
         <Navbar />
         <main>{children}</main>
--- amas-site/src/components/layout/navbar.tsx
+++ amas-site/src/components/layout/navbar.tsx
@@ -2,12 +2,21 @@ import Link from 'next/link';
 
 export default function Navbar() {
     return (
-        <nav className="sticky top-0 z-50 w-full border-b bg-orange-900">
+        <nav className="sticky top-0 z-50 w-full border-b bg-amas-orange">
             <div className="container mx-auto flex h-30 items-center justify-between px-4">
-                {/* Logo */}
-                <img src="/logo.png" alt="AMAS Logo" className="h-25 w-auto" />
-                <h1 className="text-xl font-bold tracking-tight">Association Moto Ancienne Salernoise</h1>
+                <img src="/logo.png" alt="AMAS Logo" className="h-25 w-auto" /> 
+                <h1 className="text-2xl font-bold tracking-tight">Association Moto Ancienne Salernoise</h1>
                 <img src="/blasonsalernes.webp" alt="Blason Salernes" className="h-20 w-auto" />
+            </div>
+            <div>
+                <ul className='flex justify-center space-x-6 text-xl bg-amber-900'>
+                    <li className='p-2'><Link href="/">Acceuil</Link></li>
+                    <li className='p-2'><Link href="/agenda">Agenda</Link></li>
+                    <li className='p-2'><Link href="/history">Notre histoire</Link></li>
+                    <li className='p-2'><Link href="/event">Nos sorties</Link></li>
+                    <li className='p-2'><Link href="/realisations">Nos réalisations</Link></li>
+                    <li className='p-2'><Link href="/moto">Nos motos</Link></li>
+                </ul>
             </div>
         </nav>
     );
